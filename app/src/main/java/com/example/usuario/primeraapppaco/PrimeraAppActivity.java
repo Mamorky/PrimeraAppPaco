0bf9db2fc8d54201ea977e396344cb8992698acb
mamorky <andres_ball@hotmail.com>
2017-09-26 10:40:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/usuario/primeraapppaco/PrimeraAppActivity.java
@@ -0,0 +1,34 @@
+package com.example.usuario.primeraapppaco;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class PrimeraAppActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView texto;
+    Button boton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        texto = (TextView) findViewById(R.id.textView);
+        boton = (Button) findViewById(R.id.button);
+        boton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == boton)
+            actualizar();
+    }
+
+    private void actualizar() {
+        texto.setText(new Date().toString());
+    }
+}
